9fe0590662fee6a13b403a9d280abb118846d19f
Milton Ojeda <milton@commgroup.com.ar>
2018-08-17 11:07:39 -0300

    panelcontrol grafico again

--- App/Vista/Modulos/PanelControl/v2/coordinador.php
+++ App/Vista/Modulos/PanelControl/v2/coordinador.php
@@ -57,18 +57,24 @@
     <section class="content">
       <div class="container-fluid">
       <div class="row">
-      
-        <div class="col-md-4">
-          <?php include_once PATH_VISTA."Modulos/PanelControl/Widget/GestionMensualEfectividad/coordinador.php"; ?>
-        </div>
 
-        <div class="col-md-4">
+           <div class="col-md-6">
+          <?php include_once PATH_VISTA."Modulos/PanelControl/Widget/ProgresoMensual/coordinador.php"; ?>
+        </div>
+      
+        <div class="col-md-6">
+           <div class="col-md-12">
+          <?php include_once PATH_VISTA."Modulos/PanelControl/Widget/GestionMensualEfectividad/coordinador.php"; ?>
+           </div>
+
+        <div class="col-md-12">
           <?php include_once PATH_VISTA."Modulos/PanelControl/Widget/ServiciosMensualEfectividad/coordinador.php"; ?>
         </div>
 
-        <div class="col-md-4">
+        <div class="col-md-12">
           <?php include_once PATH_VISTA."Modulos/PanelControl/Widget/Puntaje/coordinador.php"; ?>
         </div>  
+        </div>
       </div>   
     </div>
   </section>
